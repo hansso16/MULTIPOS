c3426af16705faef5edaf25b43c477a85e95ea65
Hans <hansangelocso@gmail.com>
2024-10-31 12:21:37 +0800

    Initial Commit

---
+++ MULTIPOS/src/main/java/com/soses/multilines/MultiposApplication.java
@@ -0,0 +1,13 @@
+package com.soses.multilines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiposApplication.class, args);
+	}
+
+}
---
+++ MULTIPOS/src/test/java/com/soses/multilines/MultiposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soses.multilines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
