892d1673331a34e7f895085fa014c40e9da41f83
hansso16 <hansangelocso@gmail.com>
2025-12-13 17:22:11 +0800

    prod properties

--- MULTIPOS/src/main/java/com/soses/multilines/config/SecurityConfig.java
+++ MULTIPOS/src/main/java/com/soses/multilines/config/SecurityConfig.java
@@ -3,17 +3,13 @@ package com.soses.multilines.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import com.soses.multilines.auth.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
@@ -47,19 +43,6 @@ public class SecurityConfig {
         return authConfig.getAuthenticationManager();
     }
     
-    @Bean
-    DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
-    
-    @Bean
-    UserDetailsService userDetailsService() {
-        return new UserDetailsServiceImpl();
-    }
-	
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
